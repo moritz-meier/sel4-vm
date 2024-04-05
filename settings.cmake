acede74222fbe392412ebc9b8de19c9c03d3e026
Moritz Meier <moritz.meier@dlr.de>
2024-04-05 14:38:46 +0200

    add hello-world-rootserver

---
+++ settings.cmake
@@ -0,0 +1,16 @@
+list(
+	APPEND
+	CMAKE_MODULE_PATH
+	"${CMAKE_SOURCE_DIR}/kernel"
+	"${CMAKE_SOURCE_DIR}/tools/seL4/cmake-tool/helpers")
+
+include(application_settings)
+
+correct_platform_strings()
+
+find_package(seL4 REQUIRED)
+sel4_configure_platform_settings()
+
+ApplyCommonSimulationSettings("${KernelSel4Arch}")
+ApplyCommonReleaseVerificationSettings(${RELEASE} ${VERIFICATION})
+
