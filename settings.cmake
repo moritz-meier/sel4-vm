e747b5b524ea10e437a55dc9c1e0d3f2383619c9
Moritz Meier <moritz.meier@dlr.de>
2024-04-10 13:09:42 +0200

    wip

--- settings.cmake
+++ settings.cmake
@@ -1,16 +1,19 @@
+set(project_dir "${CMAKE_CURRENT_LIST_DIR}/")
+file(GLOB project_modules ${project_dir}/projects/*)
 list(
 	APPEND
-	CMAKE_MODULE_PATH
-	"${CMAKE_SOURCE_DIR}/kernel"
-	"${CMAKE_SOURCE_DIR}/tools/seL4/cmake-tool/helpers")
+	    CMAKE_MODULE_PATH
+	    "${project_dir}/kernel"
+	    "${project_dir}/tools/seL4/cmake-tool/helpers"
+	    "${project_dir}/tools/seL4/elfloader-tool"
+	    ${project_modules})
 
 include(application_settings)
 
 correct_platform_strings()
 
-find_package(seL4 REQUIRED)
-sel4_configure_platform_settings()
+# find_package(seL4 REQUIRED)
+# sel4_configure_platform_settings()
 
 ApplyCommonSimulationSettings("${KernelSel4Arch}")
 ApplyCommonReleaseVerificationSettings(${RELEASE} ${VERIFICATION})
-
